204a3156bd9dd0dae1d2cf3ecd791538afce263f
vinicius aragao <163137558+sancier187@users.noreply.github.com>
2024-03-19 11:31:41 -0300

    Algoritmo1.java

--- Algoritmo1.java
+++ Algoritmo1.java
@@ -1,12 +1,24 @@
 
-
+import java.util.Scanner;
 public class Main {
   public static void main(String[] args) {
-
+    Scanner teclado = new Scanner( System.in );
+    // Declaracao de variaveis
     int nota=0;
-    System.out.println("Digite uma nota:")
+    System.out.println("Digite uma nota: ");
+    nota = teclado.nextInt();
+    while (nota > 10 || nota < 0){
+      
+      System.out.println("Nota invalida.Digite novamente");
+     
+      System.out.println("Digite uma nota: ");
       nota = teclado.nextInt();
-    while (nota < 10  nota >0)
+    }
+     System.out.println("Nota valida");
+   }
+
+  
+}
       
         
 
