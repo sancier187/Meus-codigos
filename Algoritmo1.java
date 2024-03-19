5b7600e6fa2fbe64f8848195d677dea40e65e80e
vinicius aragao <163137558+sancier187@users.noreply.github.com>
2024-03-19 11:26:39 -0300

    Add files via upload

---
+++ Algoritmo1.java
@@ -0,0 +1,18 @@
+
+
+public class Main {
+  public static void main(String[] args) {
+
+    int nota=0;
+    System.out.println("Digite uma nota:")
+      nota = teclado.nextInt();
+    while (nota < 10  nota >0)
+      
+        
+
+
+    
+  }
+
+  
+}
